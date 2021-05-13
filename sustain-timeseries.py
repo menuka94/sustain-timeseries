1799ba977d09eb90924b5f282601a0cd78c82341
Sustain Services Repoistory <sustain@lattice-119.cs.colostate.edu>
2021-05-12 18:33:39 -0600

    feat: load covid data from url -- attempt 1

--- sustain-timeseries.py
+++ sustain-timeseries.py
@@ -1,17 +1,86 @@
-import pandas as pd
-from prophet import Prophet
-from pymongo import MongoClient
-from prophet.plot import plot_plotly, plot_components_plotly
+import time
 
+import pandas as pd
+from pymongo import MongoClient
+from prophet import Prophet
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql.types import *
 
-from pyspark.sql import SparkSession 
+# .config("spark.jars", "org.mongodb:bson:4.0.5") \
 
-spark = SparkSession.builder.master("local").getOrCreate() 
+jars = "" \
+       "./jars/mongo-spark-connector_2.12-3.0.1.jar," \
+       "./jars/mongo-java-driver-3.12.5.jar," \
+       "./jars/bson-4.0.5.jar," \
+       "./jars/spark-core_2.12-3.0.1.jar," \
+       "./jars/spark-sql_2.12-3.0.1.jar"
+
+spark = SparkSession \
+    .builder \
+    .master("spark://lattice-100:8079") \
+    .appName("COVID-19 Time-series - PySpark") \
+    .config("spark.jars", jars) \
+    .getOrCreate()
+sqlContext = SQLContext(spark.sparkContext)
+
 print(f'Spark Version: {spark.sparkContext.version}')
 
-df = spark.read.format('json').load('./covid_county.json')
+
+# -----------------------------------------------------------------
+
+# Auxiliar functions
+# Pandas Types -> Sparks Types
+def equivalent_type(f):
+    if f == 'datetime64[ns]':
+        return DateType()
+    elif f == 'int64':
+        return LongType()
+    elif f == 'int32':
+        return IntegerType()
+    elif f == 'float64':
+        return FloatType()
+    else:
+        return StringType()
+
+
+def define_structure(string, format_type):
+    try:
+        typo = equivalent_type(format_type)
+    except:
+        typo = StringType()
+    return StructField(string, typo)
+
+
+# Given pandas dataframe, it will return a spark's dataframe
+def pandas_to_spark(df_pandas):
+    columns = list(df_pandas.columns)
+    types = list(df_pandas.dtypes)
+    struct_list = []
+    for column, typo in zip(columns, types):
+        struct_list.append(define_structure(column, typo))
+    p_schema = StructType(struct_list)
+    return sqlContext.createDataFrame(df_pandas, p_schema)
+
+
+# -----------------------------------------------------------------
+
+
+# db = MongoClient('lattice-100', 27018)
+# collection = 'covid_county_formatted'
+# cursor = db.sustaindb[collection].find()
+# df_pd = pd.DataFrame(list(cursor))
+#
+# df = pandas_to_spark(df_pd)
+
+url = 'https://bitbucket.org/menuka94/sustain-covid-county-data/raw/357e0f6b964d5c6ece880846c4d1caad1f88a65c/covid_county.json'
+from pyspark import SparkFiles
+spark.sparkContext.addFile(url)
+# df = spark.read.format('json').load('')
+df = spark.read.json(SparkFiles.get('covid_county.json'))
+
+df.cache()
 
 df = df.select('GISJOIN', 'cases', 'deaths', 'date', 'formatted_date')
 
@@ -24,6 +93,7 @@ result_schema = StructType([
     StructField("yhat_upper", DoubleType(), True)
 ])
 print('log: result_schema created')
+
 
 @pandas_udf(result_schema, PandasUDFType.GROUPED_MAP)
 def temp(df0):
@@ -42,4 +112,15 @@ df_cases.show()
 
 results = (df_cases.groupBy('GISJOIN').apply(temp))
 
+print('log: Showring results')
 results.show()
+
+time1 = time.monotonic()
+print(f'time1: {time1}')
+
+results.count()
+
+time2 = time.monotonic()
+print(f'time2: {time2}')
+
+print(f'Time Taken: {time2 - time1}')
