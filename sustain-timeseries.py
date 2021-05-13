b5eeeb229898050eb4621139d0b67145801ed772
Sustain Services Repoistory <sustain@lattice-119.cs.colostate.edu>
2021-05-12 19:57:43 -0600

    feat: update spark conf to work with Java 11

--- sustain-timeseries.py
+++ sustain-timeseries.py
@@ -1,13 +1,12 @@
 import time
 
 import pandas as pd
+import os
 from prophet import Prophet
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql.types import *
-
-# .config("spark.jars", "org.mongodb:bson:4.0.5") \
 
 jars = "" \
        "./jars/mongo-spark-connector_2.12-3.0.1.jar," \
@@ -21,6 +20,8 @@ spark = SparkSession \
     .master("spark://lattice-100:8079") \
     .appName("COVID-19 Time-series - PySpark") \
     .config("spark.jars", jars) \
+    .config("spark.driver.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true") \
+    .config("spark.executor.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true") \
     .getOrCreate()
 sqlContext = SQLContext(spark.sparkContext)
 
@@ -42,7 +43,7 @@ print('log: result_schema created')
 
 
 @pandas_udf(result_schema, PandasUDFType.GROUPED_MAP)
-def temp(df0):
+def predict(df0):
     # instantiate the model, configure the parameters
     m = Prophet()
     m.fit(df0)
@@ -56,9 +57,9 @@ df_cases = df.select('GISJOIN', 'date', 'cases').withColumnRenamed('date', 'ds')
 print('Showing df_cases')
 df_cases.show()
 
-results = (df_cases.groupBy('GISJOIN').apply(temp))
+results = (df_cases.groupBy('GISJOIN').apply(predict))
 
-print('log: Showring results')
+print('log: Showing results')
 results.show()
 
 time1 = time.monotonic()
