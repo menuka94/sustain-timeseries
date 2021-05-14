011c4ea35a4f904749c1953a609c504c4f675340
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-05-13 20:57:32 -0600

    feat: add Makefile, use env.sh, update Spark config

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	python sustain-timeseries.py
--- sustain-timeseries.py
+++ sustain-timeseries.py
@@ -15,21 +15,46 @@ jars = "" \
        "./jars/spark-core_2.12-3.0.1.jar," \
        "./jars/spark-sql_2.12-3.0.1.jar"
 
+# .config("spark.jars", jars) \
+SPARK_MASTER = os.environ["SPARK_MASTER"]
+DB_NAME = os.environ["DB_NAME"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+SPARK_THREAD_COUNT = os.environ["SPARK_THREAD_COUNT"]
+SPARK_EXECUTOR_CORES = os.environ["SPARK_EXECUTOR_CORES"]
+SPARK_EXECUTOR_MEMORY = os.environ["SPARK_EXECUTOR_MEMORY"]
+SPARK_INITIAL_EXECUTORS = os.environ["SPARK_INITIAL_EXECUTORS"]
+SPARK_MIN_EXECUTORS = os.environ["SPARK_MIN_EXECUTORS"]
+SPARK_MAX_EXECUTORS = os.environ["SPARK_MAX_EXECUTORS"]
+SPARK_BACKLOG_TIMEOUT = os.environ["SPARK_BACKLOG_TIMEOUT"]
+SPARK_IDLE_TIMEOUT = os.environ["SPARK_IDLE_TIMEOUT"]
+
+GISJOIN = "GISJOIN"
+
 spark = SparkSession \
     .builder \
-    .master("spark://lattice-100:8079") \
+    .master(SPARK_MASTER) \
     .appName("COVID-19 Time-series - PySpark") \
-    .config("spark.jars", jars) \
     .config("spark.driver.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true") \
     .config("spark.executor.extraJavaOptions", "-Dio.netty.tryReflectionSetAccessible=true") \
+    .config("park.executor.cores", SPARK_EXECUTOR_CORES) \
+    .config("spark.executor.memory", SPARK_EXECUTOR_MEMORY) \
+    .config("spark.dynamicAllocation.enabled", "true") \
+    .config("spark.dynamicAllocation.shuffleTracking.enabled", "true") \
+    .config("spark.dynamicAllocation.initialExecutors", SPARK_INITIAL_EXECUTORS) \
+    .config("spark.dynamicAllocation.minExecutors", SPARK_MIN_EXECUTORS) \
+    .config("spark.dynamicAllocation.maxExecutors", SPARK_MAX_EXECUTORS) \
+    .config("spark.dynamicAllocation.schedulerBacklogTimeout", SPARK_BACKLOG_TIMEOUT) \
+    .config("spark.dynamicAllocation.executorIdleTimeout", SPARK_IDLE_TIMEOUT) \
     .getOrCreate()
-sqlContext = SQLContext(spark.sparkContext)
 
 print(f'Spark Version: {spark.sparkContext.version}')
 
-df = spark.read.format("mongo").option("uri", "mongodb://lattice-100:27018/sustaindb.covid_county_formatted").load()
+mongo_connection_uri = f'mongodb://{DB_HOST}:{DB_PORT}/{DB_NAME}.covid_county_formatted'
+print(f'mongo_connection_uri: {mongo_connection_uri}')
+df = spark.read.format("mongo").option("uri", mongo_connection_uri).load()
 
-df = df.select('GISJOIN', 'cases', 'deaths', 'date', 'formatted_date')
+df = df.select(GISJOIN, 'cases', 'deaths', 'date', 'formatted_date')
 
 df.show()
 
@@ -53,11 +78,11 @@ def predict(df0):
     return df0_forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
 
 
-df_cases = df.select('GISJOIN', 'date', 'cases').withColumnRenamed('date', 'ds').withColumnRenamed('cases', 'y')
+df_cases = df.select(GISJOIN, 'date', 'cases').withColumnRenamed('date', 'ds').withColumnRenamed('cases', 'y')
 print('Showing df_cases')
 df_cases.show()
 
-results = (df_cases.groupBy('GISJOIN').apply(predict))
+results = (df_cases.groupBy(GISJOIN).apply(predict))
 
 print('log: Showing results')
 results.show()
